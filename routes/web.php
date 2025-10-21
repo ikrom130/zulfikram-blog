60b0d8d9955dacd541294056bc74764ad73bbcb7
ikrom130 <71263805+ikrom130@users.noreply.github.com>
2025-10-22 07:02:16 +0900

    feat: Implement PostDashboardController and update dashboard view to display posts

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PostDashboardController;
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
@@ -34,9 +35,11 @@ Route::get('/contact', function () {
     return view('contact', ['title' => 'About Page']);
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/dashboard', [PostDashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
