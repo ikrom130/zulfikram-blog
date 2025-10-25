5447fa9c9cf99e8b7d4168b0533152696cf0a978
ikrom130 <71263805+ikrom130@users.noreply.github.com>
2025-10-25 14:51:38 +0900

    fix: correct modal header title from 'Add Product' to 'Add Post'

--- resources/views/components/posts/createPost.blade.php
+++ resources/views/components/posts/createPost.blade.php
@@ -8,7 +8,7 @@
             class="flex justify-between items-center pb-4 mb-4 rounded-t border-b sm:mb-5 dark:border-gray-600"
         >
             <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                Add Product
+                Add Post
             </h3>
         </div>
         <!-- Modal body -->
